b1c6fe457e64a3c6b9745b516661132b7cd24434
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-03-23 18:03:19 +0530

    Updated currency converter to dynamically populate dropdown options and change flag images based on selection

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 // const url="fca_live_fmg3PhLA2JXyYvm94wBiFifGieaBqLSwpQlvMpRE";
 
-// const dropdowns = document.querySelectorAll(".dropdown select");
+const dropdowns = document.querySelectorAll(".dropdown select");
 
+for (let key of dropdowns) {
+  for(currCode in countryList){
+    const option = document.createElement("option");
+    option.value = currCode;
+    option.innerText = currCode;
+   
+    key.add(option);
+    }
+    key.addEventListener("change", (e)=>{
+      updateFlag(e.target);
+    });
+}
+const updateFlag = (element) => {
+    let currCode = element.value;
+  
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+  };
