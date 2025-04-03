64640518a36e97db5d98a464ee5570e8233c4dca
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-04-03 11:43:12 +0530

    API removed

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // const url="fca_live_fmg3PhLA2JXyYvm94wBiFifGieaBqLSwpQlvMpRE";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const output = document.querySelector(".msg input");
+const conversion = document.querySelector(".msg p");
+const button = document.querySelector("button");
+
+
+// conversion.innerText=`${from} to ${to}`;
+// 
 
 for (let key of dropdowns) {
   for(currCode in countryList){
@@ -11,6 +18,7 @@ for (let key of dropdowns) {
     key.add(option);
     }
     key.addEventListener("change", (e)=>{
+
       updateFlag(e.target);
     });
 }
@@ -22,3 +30,11 @@ const updateFlag = (element) => {
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
   };
+
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    output.value="45";
+  });
+
+  // Last API stops working i will try to implement it with gemini api 
+  
